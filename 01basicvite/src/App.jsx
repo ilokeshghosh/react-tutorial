70709e51cd2948374b607eac79aac8aa55ec9fb4
Lokesh Ghosh <lghosh857@gmail.com>
2023-09-13 10:56:11 +0530

    custom react

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,11 +1,35 @@
 import Youtube from "./Youtube"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const username = 'lokesh'
+
+const h1Tag = (
+  <h1>Username {"_"} is</h1>
+)
+
+const Reacth1Tag = React.createElement(
+  'h1',
+  {href: 'gogo.com'},
+  'Username',
+  " _ ",
+  'is'
+)
+
 function App() {
   return (
     <>
-    <h1>Chai aur code in vite</h1>
+    <h1>Chai aur code in vite {2+5}</h1>
+    <h1>Username is  {username}</h1>
+    <h1>Username {" "} is</h1>
+    {/* <h1>App is  {if(username === 'lokesh') ? 'yes' : 'no'}</h1> */}
+
     <Youtube/>
     </>
+    
+
   )
 }
 
 export default App
+
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -3,11 +3,56 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Youtube from './Youtube.jsx'
 
+const reactElement = {
+    type : 'a',
+    props : {
+        href: "https://google.com",
+        target: '_blank'
+    },
+    children : 'Click me to Visit Google'
+}
 
+function MyApp(){
+    return(
+        <div>
+            <h1>This is custom react function</h1>
+        </div>
+    )
+}
+
+const AnotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const areactElement = React.createElement(
+    'a', //element
+    {href : 'https://google.com', target : '_blank'}, //attributes
+    'Click to visit google' //children(innerContent/innerHTML)
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
-    <App />
-    <Youtube/>
-    </>
-  
+    // <>
+    // <App /> 
+    // <Youtube/> 
+    // </>
+
+
+    // MyApp() //not recommended
+    // <MyApp/>
+
+
+    // rendering element
+    // <AnotherElement/> //this will not work 
+    // AnotherElement //it will work
+
+    // reactElement //this will not work
+
+    // areactElement //this will work
+
+    <App/>
+
+
+
+
+
+
 )
