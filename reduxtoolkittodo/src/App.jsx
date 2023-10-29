e873601fa052adfd79da0f7e06e735bb30e82ec6
Lokesh Ghosh <lghosh857@gmail.com>
2023-10-29 18:14:57 +0530

    redux tool kit

---
+++ reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Todos, AddTodo} from './components/index'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <> 
+     <AddTodo/>
+     <Todos/>
+    </>
+  )
+}
+
+export default App
