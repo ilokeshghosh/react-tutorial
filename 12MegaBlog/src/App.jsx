d3fd44e9f8140e970cfc2132c155ba6ba4884b21
Lokesh Ghosh <lghosh857@gmail.com>
2023-11-16 18:16:03 +0530

    update : mega blog project
    configure redux toolkit in big projects

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,19 +1,39 @@
-import { useEffect, useState } from 'react'
-import {useDispatch} from 'react-redux'
-import './App.css'
-import authService  from './appwrite/auth'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import {Header,Footer } from './components/index'
 function App() {
- const[loading, setLoading] = useState(true);
- const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
- useEffect(()=>{
-})
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.log(`ERROR IN :: LOGIN :: ${error}`);
+      })
+      .finally(() => setLoading(false));
+  });
 
-  return (
-    <>
-     <h1>This is project Title</h1>
-    </>
-  )
+  return !loading ? 
+  (<div className="min-h-screen flex flex-wrap content-between bg-gray-400"> 
+  <div className="w-full block">
+    <Header/>
+    <main>
+     {/* <Outlet/> */}
+    </main>
+    <Footer/>
+  </div>
+  </div>) :( <>asdfasd</>);
 }
 
-export default App
+export default App;
