901e184ee49f06de7cf2744f9806613e7117c6d0
Lokesh Ghosh <lghosh857@gmail.com>
2024-02-20 12:28:23 +0530

    portals

---
+++ 15Portals/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PortalDemo from './PortalDemo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <><h1>thihihi</h1>
+      <PortalDemo/>
+    </>
+  )
+}
+
+export default App
